c3ee4b1e6e9d64eabf6569ea7fd32dc0b7b64a7e
binhdtqxk <146164453+binhdtqxk@users.noreply.github.com>
2025-02-13 09:48:09 +0700

    Add project

---
+++ src/main/java/com/essay/TieuLuan_BE/TieuLuanBeApplication.java
@@ -0,0 +1,13 @@
+package com.essay.TieuLuan_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TieuLuanBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TieuLuanBeApplication.class, args);
+	}
+
+}
