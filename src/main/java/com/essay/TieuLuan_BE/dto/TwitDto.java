93a15494a6c8c567a5e3c56fbd2478b73b868848
binhdtqxk <146164453+binhdtqxk@users.noreply.github.com>
2025-04-06 17:41:07 +0700

    add user,like,twit dto

---
+++ src/main/java/com/essay/TieuLuan_BE/dto/TwitDto.java
@@ -0,0 +1,35 @@
+package com.essay.TieuLuan_BE.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+@Data
+public class TwitDto {
+
+    private Long id;
+
+    private String content;
+
+    private String image;
+
+    private String video;
+
+    private UserDto user;
+
+    private LocalDateTime createdAt;
+
+    private int totalLike;
+
+    private int totalReply;
+
+    private int totalRetweets;
+
+    private boolean isLiked;
+
+    private boolean isRetwit;
+
+    private List<Long> retwitUsersId;
+
+    private List<TwitDto> replyTwits;
+}
